0ace2cbe79f03a7dc60049fc51f064432b0d503e
Kevin Wu <crazyviolindude@gmail.com>
2022-02-28 15:34:54 -0800

    Remove manual codon table

--- tcr/utils.py
+++ tcr/utils.py
@@ -13,6 +13,7 @@ import torch
 import numpy as np
 import pandas as pd
 import scipy
+import Bio.Data.CodonTable as CodonTable
 
 
 def min_power_greater_than(
@@ -195,78 +196,13 @@ def nt2aa(seq: str) -> str:
     >>> nt2aa("TGTGCCAGCAGTTTCAGGGACAGCTCCTATGAACAGTACTTC")
     'CASSFRDSSYEQYF'
     """
-    table = {
-        "ATA": "I",
-        "ATC": "I",
-        "ATT": "I",
-        "ATG": "M",
-        "ACA": "T",
-        "ACC": "T",
-        "ACG": "T",
-        "ACT": "T",
-        "AAC": "N",
-        "AAT": "N",
-        "AAA": "K",
-        "AAG": "K",
-        "AGC": "S",
-        "AGT": "S",
-        "AGA": "R",
-        "AGG": "R",
-        "CTA": "L",
-        "CTC": "L",
-        "CTG": "L",
-        "CTT": "L",
-        "CCA": "P",
-        "CCC": "P",
-        "CCG": "P",
-        "CCT": "P",
-        "CAC": "H",
-        "CAT": "H",
-        "CAA": "Q",
-        "CAG": "Q",
-        "CGA": "R",
-        "CGC": "R",
-        "CGG": "R",
-        "CGT": "R",
-        "GTA": "V",
-        "GTC": "V",
-        "GTG": "V",
-        "GTT": "V",
-        "GCA": "A",
-        "GCC": "A",
-        "GCG": "A",
-        "GCT": "A",
-        "GAC": "D",
-        "GAT": "D",
-        "GAA": "E",
-        "GAG": "E",
-        "GGA": "G",
-        "GGC": "G",
-        "GGG": "G",
-        "GGT": "G",
-        "TCA": "S",
-        "TCC": "S",
-        "TCG": "S",
-        "TCT": "S",
-        "TTC": "F",
-        "TTT": "F",
-        "TTA": "L",
-        "TTG": "L",
-        "TAC": "Y",
-        "TAT": "Y",
-        "TAA": "_",
-        "TAG": "_",
-        "TGC": "C",
-        "TGT": "C",
-        "TGA": "_",
-        "TGG": "W",
-    }
+    table = CodonTable.standard_dna_table
     seq = seq.upper()
     protein = ""
     assert len(seq) % 3 == 0
     for i in range(0, len(seq), 3):
         codon = seq[i : i + 3]
-        protein += table[codon]
+        protein += table.forward_table[codon]
     return protein
 
 
